a4f1170f687d2b5ef98473e1568aa1c7700d7645
Saud Iqbal <6971514+saudiqbal@users.noreply.github.co>
2026-02-12 14:02:37 -0500

    Quick fix

--- js/codeblock.js
+++ js/codeblock.js
@@ -69,21 +69,12 @@ CopyToClipboard(text);
 }
 
 function CopyToClipboard (text) {
-if (window.clipboardData && window.clipboardData.setData) {
-return clipboardData.setData("Text", text);
-} else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
-var textarea = document.createElement("textarea");
-textarea.textContent = text;
-textarea.style.position = "fixed";
-document.body.appendChild(textarea);
-textarea.select();
-try {
-return document.execCommand("copy");
-} catch (ex) {
-console.warn("Copy to clipboard failed.", ex);
-return false;
-} finally {
-document.body.removeChild(textarea);
-}
-}
+const textArea = document.createElement("textarea");
+textArea.value = text;
+textArea.style.position = "fixed";
+textArea.style.opacity = "0";
+document.body.appendChild(textArea);
+textArea.select();
+document.execCommand("copy");
+document.body.removeChild(textArea);
 }
