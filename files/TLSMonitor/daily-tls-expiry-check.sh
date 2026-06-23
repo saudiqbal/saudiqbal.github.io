00bad42a42040532e7384323c0a7fa2cf90a2bbf
Saud Iqbal <6971514+saudiqbal@users.noreply.github.com>
2026-06-23 16:03:34 -0400

    minor fix

--- files/TLSMonitor/daily-tls-expiry-check.sh
+++ files/TLSMonitor/daily-tls-expiry-check.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-logger -t TLSMonitor -p warning "TLS certificate expiry check start ***"
+logger -t TLSMonitor -p warning "[TLS CHECKER] Script start ***"
 date=$(date '+%Y-%m-%d %H:%M:%S')
 DAYS="14"
 DB_FILE_NAME="/var/www/html/TLSMonitor/TLS.db"
-RESULT=$(sqlite3 "${DB_FILE_NAME}" "SELECT name, value FROM domains;")
+RESULT=$(sqlite3 "${DB_FILE_NAME}" "SELECT name, value, timestamp FROM domains;")
 IFS=$'\n'
 read -d '' -ra lines <<< "$RESULT"
 for line in ${lines[@]}; do
 domainname=$(echo "$line" | cut -d'|' -f1)
 statusvalue=$(echo "$line" | cut -d'|' -f2)
-logger -t TLSMonitor -p warning "TLS certificate checking if $domainname expires in less than $DAYS days"
+timestampvalue=$(echo "$line" | cut -d'|' -f3)
+logger -t TLSMonitor -p warning "[TLS CHECKER] Checking if $domainname expires in less than $DAYS days"
 expirationdate=$(date -d "$(: | openssl s_client -connect "$domainname":443 -servername "$domainname" 2>/dev/null | openssl x509 -noout -enddate | cut -d= -f2)" +%s);
 indays=$(($(date +%s) + (86400*DAYS)));
 if [ "$indays" -gt "$expirationdate" ]; then
-logger -t TLSMonitor -p warning "TLS CHECK WARNING - Certificate for $domainname expires in less than $DAYS days, on $(date -d @"$expirationdate" '+%Y-%m-%d %H:%M:%S')"
+logger -t TLSMonitor -p warning "[TLS CHECKER] Certificate for $domainname expires in less than $DAYS days, on $(date -d @"$expirationdate" '+%Y-%m-%d %H:%M:%S')"
 # SQLite
 sqlite3 "${DB_FILE_NAME}" "UPDATE domains SET value='ER', timestamp='$expirationdate' WHERE name='$domainname';"
 # Send notification only once
@@ -23,10 +24,15 @@ if [ "${statusvalue}" = "OK" ]; then
 #curl -sSG "http://example.com/api.php?token=password" --data-urlencode "title=Certificate expires on $(date -d @"$expirationdate" '+%Y-%m-%d %H:%M:%S')" --data-urlencode "message=[$date] Certificate for $domainname expires in less than $DAYS days, on $(date -d @"$expirationdate" '+%Y-%m-%d %H:%M:%S')"
 fi
 else
-logger -t TLSMonitor -p warning "TLS CHECK OK - Certificate for $domainname expires on $(date -d @"$expirationdate" '+%Y-%m-%d %H:%M:%S')"
+logger -t TLSMonitor -p warning "[TLS CHECKER] OK - Certificate for $domainname expires on $(date -d @"$expirationdate" '+%Y-%m-%d %H:%M:%S')"
 # SQLite
+if [ "$timestampvalue" -ne "$expirationdate" ]; then
 sqlite3 "${DB_FILE_NAME}" "UPDATE domains SET value='OK', timestamp='$expirationdate' WHERE name='$domainname';"
-fi;
+logger -t TLSMonitor -p warning "[TLS CHECKER] TimeStamp change detected in $domainname, SQLite updated"
+else
+logger -t TLSMonitor -p warning "[TLS CHECKER] No TimeStamp change detected in $domainname, SQLite update skipped"
+fi
+fi
 datetoday=$(date '+%Y-%m-%d')
 expiringtoday=$(date -d @$expirationdate +'%Y-%m-%d')
 if [ "$datetoday" == "$expiringtoday" ]; then
@@ -36,5 +42,5 @@ if [ "$datetoday" == "$expiringtoday" ]; then
 fi
 done
 #unset IFS
-logger -t TLSMonitor -p warning "TLS certificate expiry check end ***"
+logger -t TLSMonitor -p warning "[TLS CHECKER] Script end ***"
 exit 0
